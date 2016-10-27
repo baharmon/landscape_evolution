fee1a287a163af19347533e12604eeb67bf68711
baharmon <baharmon@ncsu.edu>
2016-10-27 10:10:26 -0400

    bug fix

--- r.evolution.py
+++ r.evolution.py
@@ -1083,16 +1083,35 @@ class Evolution:
         # compute event-based erosivity (R) factor (MJ mm ha^-1 hr^-1)
 
         # derive rainfall energy (MJ ha^-1 mm^-1)
-        rain_energy = 0.29*(1.-(0.72*exp(-0.05*float(self.rain_intensity))))
+        #rain_energy = 0.29*(1.-(0.72*exp(-0.05*self.rain_intensity)))
+        gscript.run_command('r.mapcalc',
+            expression="{rain_energy} = 0.29*(1.-(0.72*exp(-0.05*{rain_intensity})))".format(rain_energy='rain_energy',
+                rain_intensity=self.rain_intensity),
+            overwrite=True)
 
         # derive rainfall volume (mm) = rainfall intensity (mm/hr) * (rainfall interval (min) * (1 hr / 60 min))
-        rain_volume = self.rain_intensity*(self.rain_interval/60.)
+        #rain_volume = self.rain_intensity*(self.rain_interval/60.)
+        gscript.run_command('r.mapcalc',
+            expression="{rain_volume} = {rain_intensity}*({rain_interval}/60.)".format(rain_volume='rain_volume',
+                rain_intensity=self.rain_intensity,
+                rain_interval=self.rain_interval),
+            overwrite=True)
 
         # derive event erosivity index (MJ mm ha^-1 hr^-1)
-        erosivity = (rain_energy*rain_volume)*self.rain_intensity*1.
+        #erosivity = (rain_energy*rain_volume)*self.rain_intensity*1.
+        gscript.run_command('r.mapcalc',
+            expression="{erosivity} = ({rain_energy}*{rain_volume})*{rain_intensity}*1.".format(erosivity='erosivity',
+                rain_energy=self.rain_energy,
+                rain_volume=self.rain_volume),
+            overwrite=True)
 
         # multiply by rainfall interval in seconds (MJ mm ha^-1 hr^-1 s^-1)
-        r_factor = erosivity/(self.rain_interval*60.)
+        #r_factor = erosivity/(self.rain_interval*60.)
+        gscript.run_command('r.mapcalc',
+            expression="{r_factor} = {erosivity}/({rain_interval}*60.)".format(r_factor='r_factor',
+                erosivity=erosivity,
+                rain_interval=self.rain_interval),
+            overwrite=True)
 
         # set temporary region
         gscript.use_temp_region()
@@ -1301,7 +1320,11 @@ class Evolution:
                 'smoothed_elevation',
                 'sedflow',
                 'settled_elevation',
-                'divergence'],
+                'divergence',
+                'rain_energy',
+                'rain_volume',
+                'erosivity',
+                'r_factor'],
             flags='f')
 
         return evolved_elevation, time, depth, erosion_deposition, sediment_flux, difference
@@ -1888,6 +1911,10 @@ def cleanup():
                 'smoothed_elevation',
                 'settled_elevation',
                 'divergence',
+                'rain_energy',
+                'rain_volume',
+                'erosivity',
+                'r_factor',
                 'dx',
                 'dy',
                 'dxx',
