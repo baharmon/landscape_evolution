fbc6e75cacfe0c94b44e0f795396b55de7e02085
baharmon <baharmon@ncsu.edu>
2016-10-27 10:26:43 -0400

    minor edit

--- r.evolution.py
+++ r.evolution.py
@@ -1230,18 +1230,18 @@ class Evolution:
                 density=self.density),
             overwrite=True)
 
-        # # smooth evolved elevation
-        # gscript.run_command('r.neighbors',
-        #     input=evolved_elevation,
-        #     output=smoothed_elevation,
-        #     method='average',
-        #     size=self.smoothing,
-        #     overwrite=True)
-        # # update elevation
-        # gscript.run_command('r.mapcalc',
-        #     expression="{evolved_elevation} = {smoothed_elevation}".format(evolved_elevation=evolved_elevation,
-        #         smoothed_elevation=smoothed_elevation),
-        #     overwrite=True)
+        # smooth evolved elevation
+        gscript.run_command('r.neighbors',
+            input=evolved_elevation,
+            output=smoothed_elevation,
+            method='average',
+            size=self.smoothing,
+            overwrite=True)
+        # update elevation
+        gscript.run_command('r.mapcalc',
+            expression="{evolved_elevation} = {smoothed_elevation}".format(evolved_elevation=evolved_elevation,
+                smoothed_elevation=smoothed_elevation),
+            overwrite=True)
 
         # compute second order partial derivatives of evolved elevation
         gscript.run_command('r.slope.aspect',
